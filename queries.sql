c317a857a622127cf7c1689818fefa1547f9667c
Kathryn Tucker <kathryn.tucker94@gmail.com>
2020-11-02 19:58:59 -0600

    Added SQL testing queries.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,21 @@
 ## Part 1: Test it with SQL
+id = INT
+skills = VARCHAR(250)
+employer = VARCHAR(250)
 
 ## Part 2: Test it with SQL
 
+SELECT  name
+FROM employer
+WHERE (location = "St.Louis City");
+
 ## Part 3: Test it with SQL
 
+DROP TABLE job;
+
 ## Part 4: Test it with SQL
+
+SELECT name, description, id
+FROM skill
+RIGHT JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name;
